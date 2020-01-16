d03225bec1d51e5611d40a51830764604505f1f3
ilya <ilya.kryazik10@gmail.com>
2020-01-16 17:26:24 +0200

    Add logic in Chat page

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,23 +1,94 @@
 
 let socket = new WebSocket("ws://localhost:8080");
 socket.onopen = function(event) {
-  console.log("[open] Соединение установлено");
-  console.log("Отправляем данные на сервер");
+  //console.log("[open] Соединение установлено");
+  //console.log("Отправляем данные на сервер");
 };
+$(document).ready(function () {
+  $('.toast').toast('show');
 
-socket.onmessage = function(event) {
-  alert(`[message] Данные получены с сервера: ${event.data}`);
-};
+let item = document.getElementById("dialog");
+item.scrollTop = item.scrollHeight;
 
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    alert(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
-  } else {
-    
-    alert('[close] Соединение прервано');
+});
+
+let button = document.getElementById("button_submit");
+
+button.onclick = function(event){
+  let text = document.getElementById("textarea_message");
+
+  if(text.value){
+    show_message(text.value,"Name",true);
+    socket.send(text.value);
+    text.value = "";
   }
 };
 
-socket.onerror = function(error) {
-  alert(`[error] ${error.message}`);
+socket.onmessage = function(event){
+  show_message(event.data,"Name",false);
+}
+
+socket.onclose = function(event) {
+//   if (event.wasClean) {
+//     alert(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+//   } else {
+    
+//     alert('[close] Соединение прервано');
+//   }
 };
+
+socket.onerror = function(error) {
+  //alert(`[error] ${error.message}`);
+};
+
+/**
+ * Show messages on screeen
+ * 
+ * @param {string} str Message text
+ * @param {string} name User name
+ * @param {bool} boolean Specifies which side to display the message
+ * 
+ */
+function show_message(str, name, boolean){
+
+  let item = document.getElementById("dialog");
+  let message_date = new Date();
+
+  if(name && str){
+    if(boolean === true){
+  let send_message = `<div class="row d-flex flex-row mt-3">
+<div class="col d-flex flex-row">
+  <div class="toast" data-autohide="false">
+    <div class="toast-header">
+      <strong class="mr-auto text-primary">${name}</strong>
+    </div>
+    <div class="toast-body">
+      ${str}
+    </div>
+  </div>
+</div>
+</div>`;
+
+item.insertAdjacentHTML('beforeend', send_message);
+item.scrollTop = item.scrollHeight;
+$('.toast').toast('show');
+    }
+    if(boolean === false){
+let incoming_message = `<div class="row d-flex flex-row-reverse mt-3">
+<div class="col d-flex flex-row-reverse">
+  <div class="toast" data-autohide="false">
+    <div class="toast-header">
+      <strong class="ml-auto text-primary">${name}</strong>
+    </div>
+    <div class="toast-body">
+      ${str}
+    </div>
+  </div>
+</div>
+</div>`;
+item.insertAdjacentHTML('beforeend', incoming_message);
+item.scrollTop = item.scrollHeight;
+$('.toast').toast('show');
+    }
+  }
+}
